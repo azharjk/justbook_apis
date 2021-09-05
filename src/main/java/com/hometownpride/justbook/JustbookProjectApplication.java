415e1a8d3ec1575d794ac4402204b5b2f3d35f5f
azharjk <azhr.project@gmail.com>
2021-09-06 00:03:30 +0700

    initial commit justbook_apis project

---
+++ src/main/java/com/hometownpride/justbook/JustbookProjectApplication.java
@@ -0,0 +1,11 @@
+package com.hometownpride.justbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustbookProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JustbookProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hometownpride/justbook/JustbookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hometownpride.justbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustbookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
